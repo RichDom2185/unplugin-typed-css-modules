8bf0ce3674d4ad042298e7b54c67ea5f962bf418
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-10-02 01:02:55 +0800

    Refactor to separate CSS lib file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import fs from "node:fs";
 import { glob } from "tinyglobby";
-import DtsCreator from "typed-css-modules";
-import Core from "typed-css-modules/lib/css-modules-loader-core/index.js";
 import type { UnpluginFactory } from "unplugin";
 import { createUnplugin } from "unplugin";
-import { resolveImportInconsistency } from "./lib/utils";
+import cssGenerator from "./lib/css";
 
 export type Options = {
   /**
@@ -24,15 +22,10 @@ const defaultOptions: Options = {
   scss: false,
 };
 
-const Creator = resolveImportInconsistency(DtsCreator);
-const defaultPlugins = resolveImportInconsistency(Core).defaultPlugins;
-
 const PREFIX = `/* eslint-disable */
 // WARNING: THIS FILE IS AUTO GENERATED, PLEASE DO NOT EDIT IT MANUALLY.
 // prettier-ignore
 `;
-
-const wrapContent = (content: string) => (PREFIX + content).trim() + "\n";
 
 export const unpluginFactory: UnpluginFactory<Options | undefined> = (
   options
@@ -53,12 +46,7 @@ export const unpluginFactory: UnpluginFactory<Options | undefined> = (
     fileExts.length === 1 ? fileExts[0] : `{${fileExts.join(",")}}`;
   const fileExtGlob = `**/*.module.${withBraces}`;
 
-  const dtsCreator = new Creator({
-    loaderPlugins: [
-      ...defaultPlugins,
-      // TODO: Add SCSS support via plugin?
-    ],
-  });
+  const css = cssGenerator({ banner: PREFIX });
   return {
     name: "unplugin-starter",
     async watchChange(id, { event }) {
@@ -70,19 +58,14 @@ export const unpluginFactory: UnpluginFactory<Options | undefined> = (
         }
         return;
       }
-      const content = await dtsCreator.create(id, undefined, true);
-      await content.writeFile(wrapContent);
+      await css.generate(id);
     },
     async writeBundle() {
       const dirname = process.cwd();
       const files = await glob(fileExtGlob, { cwd: dirname, absolute: true });
       const results = await Promise.allSettled(
-        files.map((file) =>
-          dtsCreator
-            .create(file, undefined, true)
-            // Fail the build if typings are out of date
-            .then((content) => content.checkFile(wrapContent))
-        )
+        // Fail the build if typings are out of date
+        files.map((file) => css.check(file))
       );
       const rejected = results.filter((r) => r.status === "rejected");
       if (rejected.length) {
---
+++ src/lib/css.ts
@@ -0,0 +1,33 @@
+import DtsCreator from "typed-css-modules";
+import Core from "typed-css-modules/lib/css-modules-loader-core/index.js";
+import { resolveImportInconsistency } from "./utils";
+
+const Creator = resolveImportInconsistency(DtsCreator);
+const defaultPlugins = resolveImportInconsistency(Core).defaultPlugins;
+
+export type CssOptions = {
+  banner?: string;
+};
+
+const css = ({ banner }: CssOptions = {}) => {
+  const wrapContent = (content: string) =>
+    ((banner ?? "") + content).trim() + "\n";
+  const dtsCreator = new Creator({
+    loaderPlugins: [
+      ...defaultPlugins,
+      // TODO: Allow supporting custom plugins?
+    ],
+  });
+  return {
+    async generate(file: string) {
+      const content = await dtsCreator.create(file, undefined, true);
+      await content.writeFile(wrapContent);
+    },
+    async check(file: string) {
+      const content = await dtsCreator.create(file, undefined, true);
+      await content.checkFile(wrapContent);
+    },
+  };
+};
+
+export default css;
